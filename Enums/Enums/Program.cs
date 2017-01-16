bcbe76dbabe335d6a8160ca333e7df780bfa343c
Lusine <lusin.khachatryan@mail.ru>
2017-01-17 03:38:10 +0400

    com4

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace ConsoleApplication15
+{
+    public enum Mast1 : byte
+    {
+        sev = 3,
+        xach,
+        sirt,
+        cyap
+
+
+
+
+
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                Console.WriteLine("enter colors Name or value");
+                string s = Console.ReadLine();
+                ConsoleColor a = 0;
+                int val;
+                if (int.TryParse(s, out val))
+                {
+                    if (Enum.IsDefined(typeof(ConsoleColor), val))
+                    {
+
+                        a = (ConsoleColor)val;
+                    }
+
+                    else
+                        Console.WriteLine("eror enter");
+
+                }
+
+                else
+                {
+                    if (Enum.IsDefined(typeof(ConsoleColor), s))
+                    {
+
+                        a = (ConsoleColor)Enum.Parse(typeof(ConsoleKey), s);
+                    }
+
+                    else
+
+                        Console.WriteLine("eror enter");
+
+                }
+                Console.BackgroundColor = a;
+                Console.Clear();
+
+
+            }
+
+            while (Console.ReadKey().Key == ConsoleKey.Enter);
+        }
+
+
+
+    }
+}
