6d09e17ee6e96e45e7db528e61cedb7f18593937
Lusine <lusin.khachatryan@mail.ru>
2017-02-03 18:32:22 +0400

    com7

---
+++ Delegate/Delegates/Car.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate int MD(int a, int b);
+    // public delegate void CarEventHandler(string s);
+
+  
+    class Car
+    {
+        private bool isCrash;
+        private EventHandler carCrash;
+        private EventHandler carDanger;
+        public void RegisterCarCrash(EventHandler cch)
+        {
+            carCrash += cch;
+        }
+        public void UnRegisterCarCrash(EventHandler cch)
+        {
+            carCrash -= cch;
+        }
+        //======================================================
+        public void RegisterDanger(EventHandler cch)
+        {
+            carDanger += cch;
+        }
+        public void UnRegisterDanger(EventHandler cch)
+        {
+            carDanger -= cch;
+        }
+
+        public bool IsCrash
+        {
+            get { return isCrash; }
+            set { isCrash = value; }
+        }
+
+
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        private readonly int maxSpeed;
+
+        public void accelerate(int a)
+        {
+            if (IsCrash)
+            {
+                return;
+            }
+            else
+            {
+                Speed += a;
+            }
+            if (Speed >= MaxSpeed)
+            {
+                IsCrash = true;
+                if (carCrash != null)
+                {
+                    carCrash(this,EventArgs.Empty);
+                }
+                
+                // hnaravorutyun tal tiroje ardzaganqel
+            }
+            else if (Speed + 10 > MaxSpeed)
+            {
+                if (carDanger !=null)
+                {
+                    carDanger(this, EventArgs.Empty);                    
+                }
+                // hnaravorutyun tal tiroje ardzaganqel
+            }
+            else
+            {
+                Console.WriteLine(this);
+            }
+        }
+
+        public int MaxSpeed
+        {
+            get { return maxSpeed; }
+        }
+   
+        public Car(string nm = "Ford", int sp = 50, int mxSp = 250)
+        {
+            Name = nm;
+            sp = Speed;
+            maxSpeed = mxSp;
+        }
+     
+        public override string ToString()
+        {
+            return Name + "\t" + Speed + "\t" + maxSpeed;
+        }
+    }
+}
---
+++ Delegate/Delegates/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        public static void OnCarCrash1(Object o, EventArgs crash)
+        {
+            if (o is Car)
+            {
+                Car c = o as Car;
+                c.Speed = 0;
+                c.IsCrash = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("is this car {0}", c.Name);
+                Console.ResetColor();
+            }
+        }
+        public static void OnDanger(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Car close to the maximum speed");
+            Console.ResetColor();
+        }
+        public static void Emergency(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Exceed the maximum speed of the car");
+            Console.ResetColor();
+        }
+        static void Main(string[] args)
+        {
+           
+            var c1 = new Car("Ford", 89, 250);
+            var c2 = new Car("BMW", 120, 180);
+          
+
+            c1.RegisterCarCrash(OnCarCrash1);
+
+            for (int i = 0; i < 100; i++)
+            {
+                c1.accelerate(10);
+                c2.accelerate(11);
+            }
+          
+            Console.ReadLine();
+        }
+    }
+}
