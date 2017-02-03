25a5a5c7e7dfd8f2294f5990b7259b04a42ab8b3
Lusine <lusin.khachatryan@mail.ru>
2017-02-03 18:57:45 +0400

    com8

---
+++ Multithreading/Multithreading/Class.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging; // used this
+using System.Text;
+using System.Threading;
+
+
+
+namespace async
+{
+    class Class
+    {
+        public static void FinallyHandler(IAsyncResult iar)
+        {
+            Console.WriteLine("Thread ID = " + Thread.CurrentThread.ManagedThreadId);
+            AsyncResult ar = iar as AsyncResult;
+            MD d = ar.AsyncDelegate as MD;
+            int result = d.EndInvoke(iar);
+            Console.WriteLine("Res is {0}",result);
+            Console.WriteLine(((DateTime)ar.AsyncState).ToLongTimeString()); // datetime
+            
+        }
+        public static int MyMethod(int n)
+        {
+            int sum = 0;
+            Console.WriteLine("Thread ID = " + Thread.CurrentThread.ManagedThreadId);
+            while (n>0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Num from mymethod {0}",n--);
+                Thread.Sleep(500);
+                Console.ResetColor();
+                sum += n;
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Finish MyMethod");
+            Console.ResetColor();
+            return sum;
+        }
+    }
+}
---
+++ Multithreading/Multithreading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading; //used
+
+namespace async
+{
+    public delegate int MD(int a);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thread ID = " + Thread.CurrentThread.ManagedThreadId);
+          
+            MD md = Class.MyMethod; 
+           
+            IAsyncResult iar = md.BeginInvoke(15, Class.FinallyHandler,DateTime.Now);
+            int m = 30;
+            while (m>0)
+            {
+              
+                Console.WriteLine("Num from Main {0}",m);
+                Console.ResetColor();
+                Thread.Sleep(700);
+                m--;
+            }
+            
+            Console.WriteLine("Finish Main");
+          
+        }
+    }
+}
